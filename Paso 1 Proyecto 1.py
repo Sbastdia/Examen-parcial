c2ff65a5797d09d47093ecfae3dc224a132aee44
Albertojserr <alberto.j.serrano@gmail.com>
2021-11-15 16:57:39 +0100

    Cambio separadores

--- Paso 1 Proyecto 1.py
+++ Paso 1 Proyecto 1.py
@@ -1,3 +1,3 @@
 import pandas as pd
-conversiones=pd.read_csv("conversiones.csv")
-navegacion=pd.read_csv("navegacion.csv")
+conversiones=pd.read_csv("conversiones.csv", sep=";")
+navegacion=pd.read_csv("navegacion.csv", sep=";")
