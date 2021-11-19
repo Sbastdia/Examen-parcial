a47ad9cdeaf7a8bd2673d9492515c10101a81ab3
Albertojserr <alberto.j.serrano@gmail.com>
2021-11-19 12:58:24 +0100

    Actualización buena de paso 2

--- paso2proyecto1.py
+++ paso2proyecto1.py
@@ -10,9 +10,15 @@ conv=pd.DataFrame(conversiones)
 def separarUrl(URL,palabraclave):
     url_parts=URL.split("&")
     camp=0
+    ar=[]
     for part in url_parts:
         if "=" in part:
-            key, value=part.split("=")
+            ar=part.split("=")
+            if(len(ar)>2):
+                return -1
+            else:
+                key=ar[0]
+                value=ar[1]
             #print(key,value)
             if key==palabraclave:
                 camp=value
@@ -24,24 +30,27 @@ def sacarunaURL(Dataframe,i):
     url=Dataframe.iloc[i]["url_landing"]
     return url
 
-def separarUrlColumna(Dataframe,palabraclave):
-    Campa=[]
-    for i in range (0,len(Dataframe)):
-        camp=separarUrl(sacarunaURL(Dataframe,i),palabraclave)
-        #print(type(Campa))
-        Campa.append(camp)
-    return Campa
-
 def separarUrlColumnaDDF(Dataframe,palabraclave):
-    Campa=pd.DataFrame(columns=[palabraclave])
-    for i in range (0,len(Dataframe)):
-        camp=separarUrl(sacarunaURL(Dataframe,i),palabraclave)
-        #print(type(Campa))
-        Campa=Campa.append({palabraclave:camp}, ignore_index=True)
+    Campa=pd.DataFrame(columns=["campaña"])
+    Data2=Dataframe
+    borrados=0
+    for i in range (0,len(Data2)):
+        camp=separarUrl(sacarunaURL(Data2,i),palabraclave)
+        if(camp==-1):
+            Dataframe=Dataframe.drop(i-borrados)
+            borrados+=1
+        else:
+            #print(type(Campa))
+            Campa=Campa.append({'campaña':camp}, ignore_index=True)
     return Campa
-nav3=nav.loc[1:100]
-#camp,adg, adv, sl
-Campaña=separarUrlColumnaDDF(nav3,"camp")
-adgroup=separarUrlColumnaDDF(nav3,"adg")
-Campaf=nav3.assign(Campaña=Campaña["campaña"])
-print(Campaf.head)
+print(len(nav))
+nav.dropna(subset=["url_landing"], inplace=True)
+print(len(nav))
+#nav3=nav.loc[7149:7160]
+# #camp,adg, adv, sl
+Campaña=separarUrlColumnaDDF(nav,"camp")
+# adgroup=separarUrlColumnaDDF(nav3,"adg")
+Campaña.to_csv("Campaña.csv")
+#Campaf=nav.assign(Campaña=Campaña["campaña"])
+#print(Campaf.head)
+#Campaf.to_csv("Campaf.csv")
