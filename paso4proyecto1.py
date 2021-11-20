7e0871f7e57372a831d99ff651924c85d4c45a2e
Albertojserr <alberto.j.serrano@gmail.com>
2021-11-20 10:07:50 +0100

    Paso 4 proyecto 1 cambios

--- paso4proyecto1.py
+++ paso4proyecto1.py
@@ -1,8 +1,8 @@
 import pandas as pd
 def juntarTablas(Dataframe1,Dataframe2):
     if "id_suite" in Dataframe1.columns() and "id_suite" in Dataframe2.columns():
-        pd.merge(Dataframe1, Dataframe2, on='id_suite', how='outer')
+        pd.merge(Dataframe1, Dataframe2, on='id_suite', how='outer',suffixes=("_nav","_conv"))
     elif "gclid" in Dataframe1.columns() and "gclid" in Dataframe2.columns():
-        pd.merge(Dataframe1, Dataframe2, on='gclid', how='outer')
+        pd.merge(Dataframe1, Dataframe2, on='gclid', how='outer',suffixes=("_nav","_conv"))
     else:
-        pd.merge(Dataframe1, Dataframe2, on='url_landing', how='outer')
+        pd.merge(Dataframe1, Dataframe2, on='url_landing', how='outer',suffixes=("_nav","_conv"))
